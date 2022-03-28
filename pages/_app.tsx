cf122b6e82b54031f084effd571d2197049f7cdf
jeina7 <jane1231001@gmail.com>
2022-03-29 02:30:35 +0900

    added plausible analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import PlausibleProvider from "next-plausible";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import Head from "next/head";
@@ -9,52 +10,55 @@ const App = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <link
-            rel="preload"
-            href="/fonts/Inter/Inter-Bold.woff2"
-            as="font"
-            crossOrigin="anonymous"
+          rel="preload"
+          href="/fonts/Inter/Inter-Bold.woff2"
+          as="font"
+          crossOrigin="anonymous"
         />
         <link
-            rel="preload"
-            href="/fonts/Inter/Inter-Light.woff2"
-            as="font"
-            crossOrigin="anonymous"
-        /> 
-        <link
-            rel="preload"
-            href="/fonts/Inter/Inter-ExtraBold.woff2"
-            as="font"
-            crossOrigin="anonymous"
+          rel="preload"
+          href="/fonts/Inter/Inter-Light.woff2"
+          as="font"
+          crossOrigin="anonymous"
         />
         <link
-            rel="preload"
-            href="/fonts/Inter/Inter-Medium.woff2"
-            as="font"
-            crossOrigin="anonymous"
-        />
-         <link
-            rel="preload"
-            href="/fonts/SpoqaHanSansNeo/SpoqaHanSansNeo-Thin.woff2"
-            as="font"
-            crossOrigin="anonymous"
+          rel="preload"
+          href="/fonts/Inter/Inter-ExtraBold.woff2"
+          as="font"
+          crossOrigin="anonymous"
         />
         <link
-            rel="preload"
-            href="/fonts/SpoqaHanSansNeo/SpoqaHanSansNeo-Bold.woff2"
-            as="font"
-            crossOrigin="anonymous"
+          rel="preload"
+          href="/fonts/Inter/Inter-Medium.woff2"
+          as="font"
+          crossOrigin="anonymous"
         />
         <link
-            rel="preload"
-            href="/fonts/SpoqaHanSansNeo/SpoqaHanSansNeo-Light.woff2"
-            as="font"
-            crossOrigin="anonymous"
+          rel="preload"
+          href="/fonts/SpoqaHanSansNeo/SpoqaHanSansNeo-Thin.woff2"
+          as="font"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/SpoqaHanSansNeo/SpoqaHanSansNeo-Bold.woff2"
+          as="font"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/SpoqaHanSansNeo/SpoqaHanSansNeo-Light.woff2"
+          as="font"
+          crossOrigin="anonymous"
         />
       </Head>
+
       <ThemeProvider attribute="class">
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <PlausibleProvider domain="jeina.io">
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </PlausibleProvider>
       </ThemeProvider>
     </>
   );
